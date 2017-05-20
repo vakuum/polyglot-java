8b85dc5074e9ecd156a690df50f4e45ccd4f6c28
Clemens Fuchslocher <clemens@vakuumverpackt.de>
2017-05-20 17:13:45 +0200

    Updated Frege version.

--- frege/src/main/java/polyglot/HelloFrege.java
+++ frege/src/main/java/polyglot/HelloFrege.java
@@ -2,12 +2,9 @@ package polyglot;
 
 import frege.Prelude;
 import frege.prelude.PreludeBase;
-import frege.runtime.Delayed;
 
 public class HelloFrege {
 	public void hello(String name) {
-		Delayed.forced(
-			PreludeBase.TST.performUnsafe(polyglot.frege.HelloFrege.hello(name))
-		);
+		PreludeBase.TST.performUnsafe(polyglot.frege.HelloFrege.hello(name)).call();
 	}
 }
